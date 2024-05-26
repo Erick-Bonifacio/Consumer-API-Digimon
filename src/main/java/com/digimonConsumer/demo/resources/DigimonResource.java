6ac6a88cca5c92d91ba340ccb975a965c3a83bc2
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-05-26 16:49:49 -0300

    Endpoint findAll Digimons

--- src/main/java/com/digimonConsumer/demo/resources/DigimonResource.java
+++ src/main/java/com/digimonConsumer/demo/resources/DigimonResource.java
@@ -1,5 +1,27 @@
 package com.digimonConsumer.demo.resources;
 
-public class DigimonResource {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.digimonConsumer.demo.entitiesDto.DigimonDto;
+import com.digimonConsumer.demo.services.DigimonService;
+
+@RestController
+@RequestMapping(value="/digimons")
+public class DigimonResource {
+	
+	@Autowired
+	private DigimonService service;
+	
+	@GetMapping
+	public ResponseEntity<List<DigimonDto>> findAll(){
+		List<DigimonDto> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
